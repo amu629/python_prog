28714be8efbf9af2354c813df4726ddf7a7bdb61
root <root@amu629.com>
2019-05-28 23:03:00 +0530

    fullway_chatbox_w/o_threading

--- udp_receiver.py
+++ udp_receiver.py
@@ -14,10 +14,15 @@ s.bind(ip_port)
 
 Buffer_Size=100
 
+#Senders receiving Socket
+ip_ports=("127.0.0.1",9000)
+
 i=1
 while i<12:
 	rec_data=s.recvfrom(Buffer_Size)
-	print rec_data
+	print rec_data[0]
+	msg=raw_input("Enter Your Message : ")
+	s.sendto(msg,ip_ports)
 	i+=1
 print 'connection closed'
 s.close()
--- udp_sender.py
+++ udp_sender.py
@@ -7,13 +7,20 @@ import socket
 #To call The Socket Object
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-#Specifying IP and Port of receiver
+#Specifying receivers receiving port
 ip_port=("127.0.0.1",9090)
+
+#Specifying senders receiving port
+ip_ports=("127.0.0.1",9000)
+s.bind(ip_ports)
+buffer_size=100
 
 i=1
 while i<12:
 	msg=raw_input("Enter Your MESSAGE : ")
 	s.sendto(msg,ip_port)
+	rec_data=s.recvfrom(buffer_size)
+	print rec_data[0]
 	i+=1
 print "connection ended"
-
+s.close()
